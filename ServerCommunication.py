5ffc19a84fade68823b9c8cd5574f3b5f53b6955
Florian <florinor36@hotmail.com>
2026-04-12 12:36:11 +0200

    pong update

--- ServerCommunication.py
+++ ServerCommunication.py
@@ -3,45 +3,43 @@ import struct
 import json
 
 # send the connection message in TCP
-s = socket.socket()
 serverAddress = ('172.17.10.41', 3000)  #ip and port of teacher
-s.connect(serverAddress)
+myAdress = ('0.0.0.0', 8888)    #my IP and listening port
 
-Dict ={
+bot_data ={
   "request": "subscribe",
   "port": 8888,
   "name": "SexyWizard",
   "matricules": ["24092", "24087"]
 }
 
-message = json.dumps(Dict).encode()
+s = socket.socket()
+s.connect(serverAddress)
+message = json.dumps(bot_data).encode()
 s.send(struct.pack("I", len(message)))
 s.send(message)
-print(s.recv(32))
+print(s.recv(2).decode())   #receive 'ok' to confirm connection with bot_data information
 
 #listen ping and answer pong in TCP
-ls = socket.socket()
-myAdress = ('0.0.0.0', 8888)        #my IP and listening port
-ls.bind(myAdress)
-ls.settimeout(0.5)
-ls.listen()
+#(desactiver les deux perfeux windows defender dans pare-feu windows defender pour recevoir les messages)
+pps = socket.socket()
+pps.bind(myAdress)
+pps.listen()
+pps.settimeout(0.5)
 
-#desactiver les deux perfeux windows defender dans pare-feu windows defender pour recevoir les messages
 while True:
-    try: 
-        client, adress = ls.accept()
+    try:
+        client, address = pps.accept()
         with client:
-            message = client.recv(4)
-            message.decode()
-            print(message)      #recieve ping in json (ok)
-            #if ping then send pong message in json
-            if json.loads(message) == "ping":  #ERROR I just want the word ping 
-                pong = {
+            ping_message = client.recv(4).decode()  #receive ping
+            print(ping_message)
+            if ping_message == "ping":
+                pong_data = {
                       "response": "pong"
                 }
-                message = json.dumps(pong).encode()
-                s.send(struct.pack("I", len(message)))
-                s.send(message)
+                pong_message = json.dumps(pong_data).encode()
+                client.send(struct.pack("I", len(pong_message)))
+                client.send(pong_message)   #send pong
     except socket.timeout:
         pass
 
