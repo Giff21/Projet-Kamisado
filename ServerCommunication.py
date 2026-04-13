a1e790bab43bd1dcb41c4f3c5a3a08d7862e0f26
Florian <florinor36@hotmail.com>
2026-04-13 13:44:31 +0200

    pong ready update

--- ServerCommunication.py
+++ ServerCommunication.py
@@ -3,25 +3,25 @@ import struct
 import json
 
 # send the connection message in TCP
-serverAddress = ('172.17.10.41', 3000)  #ip and port of teacher
+serverAddress = ('10.28.157.176', 3000)  #ip and port of teacher
 myAdress = ('0.0.0.0', 8888)    #my IP and listening port
 
 bot_data ={
-  "request": "subscribe",
-  "port": 8888,
-  "name": "SexyWizard",
-  "matricules": ["24092", "24087"]
+    "request": "subscribe",
+    "port": 8888,
+    "name": "SexyWizard",
+    "matricules": ["24092", "24087"]
 }
 
 s = socket.socket()
 s.connect(serverAddress)
-message = json.dumps(bot_data).encode()
+message = json.dumps(bot_data).encode('utf-8')
 s.send(struct.pack("I", len(message)))
 s.send(message)
-print(s.recv(2).decode())   #receive 'ok' to confirm connection with bot_data information
+print(s.recv(32).decode('utf-8'))   #receive 'ok' to confirm connection with bot_data information
 
 #listen ping and answer pong in TCP
-#(desactiver les deux perfeux windows defender dans pare-feu windows defender pour recevoir les messages)
+#(desactiver les deux pare-feux windows defender dans pare-feux windows defender pour recevoir les messages)
 pps = socket.socket()
 pps.bind(myAdress)
 pps.listen()
@@ -31,15 +31,16 @@ while True:
     try:
         client, address = pps.accept()
         with client:
-            ping_message = client.recv(4).decode()  #receive ping
+            ping_message = client.recv(32).decode('utf-8')  #receive ping
             print(ping_message)
-            if ping_message == "ping":
+            if "ping" in ping_message:
                 pong_data = {
-                      "response": "pong"
+                    "response": "pong"
                 }
-                pong_message = json.dumps(pong_data).encode()
+                pong_message = json.dumps(pong_data).encode('utf-8')
                 client.send(struct.pack("I", len(pong_message)))
                 client.send(pong_message)   #send pong
+                print(pong_data)
     except socket.timeout:
         pass
 
