0f5ab9c033b5ebb830fead92ac3a31dcfd3d9170
Florian <florinor36@hotmail.com>
2026-04-22 17:39:10 +0200

    pongMessage function created

--- communication.py
+++ communication.py
@@ -3,8 +3,8 @@ import json
 import struct
 
 def inscription(IPadress: str, clientPort: int, AIname: str, matricule: list[int,int], serverPort: int):
-    """
-    register our info to the game server
+    """Return nothing
+    descr : register our info to the game server
     """
 
     inscription_data = {
@@ -22,11 +22,24 @@ def inscription(IPadress: str, clientPort: int, AIname: str, matricule: list[int
         response = s.recv(32).decode('utf-8')
         print(response) #should recieve 'OK' form the server
 
+
+def pongMessage() -> json:
+    """Return the encoded json with "pong" """
+
+    pong_data = {
+    "response": "pong"
+    }
+
+    return json.dumps(pong_data).encode('utf-8')
+
+
 def moveMessage(move: list) -> json:
     """Return the encoded json with the move to do
 
-    take the move to do and construct the tamplate to send
+    param : list of the move we want to make
+    descr : take the move to do and construct the tamplate to send
     """
+
     move_data = {
     "response": "move",
     "move": move,
@@ -34,3 +47,5 @@ def moveMessage(move: list) -> json:
     }
 
     return json.dumps(move_data).encode('utf-8')
+
+
