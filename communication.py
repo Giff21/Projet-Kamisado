65345f8dbd908919ede598fc4a82a5dbc1a0d0ac
Florian <florinor36@hotmail.com>
2026-04-22 18:58:02 +0200

    function for the server communication created

--- communication.py
+++ communication.py
@@ -2,7 +2,8 @@ import socket
 import json
 import struct
 
-def inscription(IPadress: str, clientPort: int, AIname: str, matricule: list[int,int], serverPort: int):
+
+def inscription( AIname: str, matricule: list[int,int], servIPadress: str, clientPort=8888, serverPort=3000):
     """Return nothing
     descr : register our info to the game server
     """
@@ -15,7 +16,7 @@ def inscription(IPadress: str, clientPort: int, AIname: str, matricule: list[int
     }
 
     with socket.socket() as s:
-        s.connect((IPadress, serverPort))
+        s.connect((servIPadress, serverPort))
         message = json.dumps(inscription_data).encode('utf-8')
         s.send(struct.pack("I", len(message)))
         s.send(message)
@@ -33,19 +34,57 @@ def pongMessage() -> json:
     return json.dumps(pong_data).encode('utf-8')
 
 
-def moveMessage(move: list) -> json:
+def moveMessage(boardState: dict) -> json:
     """Return the encoded json with the move to do
 
-    param : list of the move we want to make
-    descr : take the move to do and construct the tamplate to send
+    agr : dict of the board state
+    descr : take board use the move function to find a moveToPlay and construct the tamplate to send
     """
+
+    moveToPlay = move(boardState)
 
     move_data = {
     "response": "move",
-    "move": move,
+    "move": moveToPlay,
     "message": "Fun message"
     }
 
     return json.dumps(move_data).encode('utf-8')
 
 
+def serverCom(clientPort=8888):
+    """Return nothing
+
+    descr : communicate with the server - send pong if ping recieved then, take the board state and send the move
+    
+    """
+    with socket.socket as ls:
+        ls.bind(('0.0.0.0', clientPort))
+        ls.listen()
+        ls.settimeout(0.5)
+        while True:
+            try:
+                client, address = ls.accept()
+                with client:
+                    len_info = client.recv(4)
+                    len_mes = struct.unpack("I", len_info)[0]
+                    client_message = client.recv(len_mes).decode('utf-8')  #sould receive ping
+                    client_message_dict = json.loads(client_message)
+                    print(client_message_dict)
+
+                    if client_message_dict["request"] == "ping":
+                        client.send(struct.pack("I", len(pongMessage())))
+                        client.send(pongMessage()) #sould send pong
+                        print(json.loads(pongMessage()))
+
+                    elif client_message_dict["request"] == "play":
+                        boardState = client_message_dict["state"]
+                        client.send(struct.pack("I", len(moveMessage(boardState))))
+                        client.send(moveMessage(boardState)) #sould send the move
+                        
+                        if client_message_dict["errors"] != "":
+                            print(client_message_dict["errors"])
+                client.close()
+
+            except socket.timeout:
+                pass
