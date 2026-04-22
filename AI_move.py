0df27769087931cc11ffe887a1ee599e22810046
Florian <florinor36@hotmail.com>
2026-04-22 21:15:29 +0200

    update instructions

--- AI_move.py
+++ AI_move.py
@@ -1,6 +1,8 @@
 #1)find all the available tiles
-#2)find the best move from the available tiles -> return a list [final_rows, final_column]
+#2)move to a random available tile -> return a list [final_rows, final_column]
 #3)put in common [[initial_rows, initial_column], [final_rows, final_column]]
+#4)find the best move from the available tiles: the closest to the ennemi end without giving the win in the next round -> return a list [final_rows, final_column]
+
 from Pawn_finder import FindPawn
 
 def PossibleMove(boardState):
@@ -10,4 +12,5 @@ def Minamax() -> list:
     pass
 
 def move(boardState) -> list:
+    Minamax(PossibleMove(boardState))
     return [FindPawn, Minamax()]
