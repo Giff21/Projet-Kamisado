e01efd30d5d9afd62944a79a6e45ecfa010832cd
Florian <florinor36@hotmail.com>
2026-04-22 20:42:33 +0200

    commented what will be in this file

--- AI_move.py
+++ AI_move.py
@@ -1,2 +1,13 @@
-def move():
+#1)find all the available tiles
+#2)find the best move from the available tiles -> return a list [final_rows, final_column]
+#3)put in common [[initial_rows, initial_column], [final_rows, final_column]]
+from Pawn_finder import FindPawn
+
+def PossibleMove(boardState):
     pass
+
+def Minamax() -> list:
+    pass
+
+def move(boardState) -> list:
+    return [FindPawn, Minamax()]
