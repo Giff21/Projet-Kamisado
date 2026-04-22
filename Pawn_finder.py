e2b1edc698ad62c018fca07aec2c5d00eca99c36
Florian <florinor36@hotmail.com>
2026-04-22 20:43:17 +0200

    creadted a file to find the pawn to play

---
+++ Pawn_finder.py
@@ -0,0 +1,5 @@
+# JF, you can put your stuff here in order to find the tile to play
+#the fonction must return a list [initial_rows, initial_column]
+
+def FindPawn(boardState) -> list[int, int]:
+    pass
