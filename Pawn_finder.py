7c6716c81f94c82f53048e553b7ddc09860be281
Florian <florinor36@hotmail.com>
2026-04-22 21:15:39 +0200

    update instruction

--- Pawn_finder.py
+++ Pawn_finder.py
@@ -1,5 +1,7 @@
 # JF, you can put your stuff here in order to find the tile to play
-#the fonction must return a list [initial_rows, initial_column]
+#1)find the position of all the pawn, light or dark and if we are dark(0) or light(1)
+#2)the fonction must return a list of the pawn we want to play [initial_rows, initial_column] first and then the pawn we must play according to the color the enemy ended
+#3)later give me the color of the ennemis to pervent Flo to tell our pawn to go on the color that will get the will to the ennemi
 
 def FindPawn(boardState) -> list[int, int]:
     pass
