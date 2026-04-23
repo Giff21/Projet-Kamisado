f1c0a560ddeda6740dc050e69a58c82f75fa56ed
Florian <florinor36@hotmail.com>
2026-04-23 16:09:04 +0200

    specify client port

--- AI_starter.py
+++ AI_starter.py
@@ -1,9 +1,10 @@
 from communication import inscription, serverCom
 
-serverIP = "172.17.10.125"
+serverIP = "172.17.10.54"
+clientPort = 8888
 
 if __name__ == "__main__":
-    inscription("Hello", [11111,22222], serverIP)
-    serverCom()
+    inscription("Hello", [11111,22222], serverIP, clientPort)
+    serverCom(clientPort)
 
 
