663ba3006848a04b9548760fb5c297adb1f069b1
Florian <florinor36@hotmail.com>
2026-04-23 13:59:51 +0200

    change IP

--- AI_starter.py
+++ AI_starter.py
@@ -1,6 +1,6 @@
 from communication import inscription, serverCom
 
-serverIP = "192.168.1.27"
+serverIP = "172.17.10.125"
 
 if __name__ == "__main__":
     inscription("Hello", [11111,22222], serverIP)
