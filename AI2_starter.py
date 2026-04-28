7b4369f696ca6873a610e22299bd85ee5c9d176e
Florian <florinor36@hotmail.com>
2026-04-28 18:20:52 +0200

    changed name

--- AI2_starter.py
+++ AI2_starter.py
@@ -2,9 +2,9 @@ from communication import inscription, server_communication
 
 serverIP = "192.168.129.11"
 clientPort = 8889
-smart = True
+smart = False
 time_limit = 2.5  # max3s on 8 games loss 1 time
 
 if __name__ == "__main__":
-    inscription("Hello2", [33333, 44444], serverIP, clientPort)
+    inscription("RANDOM", [33333, 44444], serverIP, clientPort)
     server_communication(clientPort, smart, time_limit)
--- AI_starter.py
+++ AI_starter.py
@@ -2,9 +2,9 @@ from communication import inscription, server_communication
 
 serverIP = "192.168.129.11"
 clientPort = 8888
-smart = False
+smart = True
 time_limit = 2.5  # max 3s
 
 if __name__ == "__main__":
-    inscription("Hello", [11111, 22222], serverIP, clientPort)
+    inscription("SMART", [11111, 22222], serverIP, clientPort)
     server_communication(clientPort, smart, time_limit)
