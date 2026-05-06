76c97644ce2c48d8bddbb449358046f13af70446
Florian <florinor36@hotmail.com>
2026-05-06 23:04:23 +0200

    cleaned negamax

--- src/negamax.py
+++ src/negamax.py
@@ -27,7 +27,7 @@ def winner(dark_pos: list, light_pos: list) -> int:
 
 
 def heurestic(dark_pos: list, light_pos: list, player: int, list_move: list) -> int:
-    """find the shortest distance from the winning positions
+    """score to find the shortest distance from the winning positions
 
     Args:
         dark_pos (list): positions of all dark pawns
@@ -36,7 +36,7 @@ def heurestic(dark_pos: list, light_pos: list, player: int, list_move: list) -> 
         list_move (list): all available move from the pawn tile
 
     Returns:
-        int: positive I'm closest to winning, negative ennemy is closest to winning
+        int: score
     """
     if game_over(dark_pos, light_pos, list_move):
         the_winner = winner(dark_pos, light_pos)
@@ -46,7 +46,6 @@ def heurestic(dark_pos: list, light_pos: list, player: int, list_move: list) -> 
             return 9  # victory is +9 points
         return -9
 
-    # calculate the distance from winning
     dark_rows = [pos[0] for pos in dark_pos]
     light_rows = [pos[0] for pos in light_pos]
     if player == 0:
@@ -56,16 +55,16 @@ def heurestic(dark_pos: list, light_pos: list, player: int, list_move: list) -> 
         my_distance = 7 - max(light_rows)
         enemy_distance = min(dark_rows)
 
-    return enemy_distance - my_distance
+    return enemy_distance - my_distance  # positive I'm closest to winning, negative ennemy is closest to winning
 
 
 def game_over(dark_pos: list, light_pos: list, list_move: list) -> bool:
-    """check if the game is over: has a winner or is blocked
+    """check if the game has a winner or is blocked
 
     Args:
         dark_pos (list): dark pawns positions
         light_pos (list): light panws positions
-        list_move (list): all the current moves
+        list_move (list): all available move from the pawn tile
 
     Returns:
         bool: True=game is over, False=game not over
@@ -97,16 +96,14 @@ def apply(boardState: dict, move: list, player: int, pawn: list) -> dict:
     new_row, new_col = move
     old_row, old_col = pawn
 
-    # take the info of the starting tile and the final tile, tile is ["piece_color","pawn_side"]
-    old_cell_color, old_tile = new_board[old_row][old_col]
-    new_cell_color, new_tile = new_board[new_row][new_col]
+    # take the info of the two tiles that will be modified
+    old_cellcolor, old_tile = new_board[old_row][old_col]
+    new_cellcolor, new_tile = new_board[new_row][new_col]
 
-    # erase the pawn in the old position and write it in the new position
-    new_board[old_row][old_col] = [old_cell_color, None]
-    new_board[new_row][new_col] = [new_cell_color, old_tile]
-
-    # write the new cell_color and change plyer turn
-    new_state["color"] = new_cell_color
+    # erase and write new positions
+    new_board[old_row][old_col] = [old_cellcolor, None]
+    new_board[new_row][new_col] = [new_cellcolor, old_tile]
+    new_state["color"] = new_cellcolor
     new_state["current"] = 1 - player
 
     return new_state
@@ -128,7 +125,7 @@ def negamax(
         list_move (list): all the available moves
         start_time (float): start of the timer
         time_limit (float): maximum time a play can last (max 3s)
-        depth (int): how much further play we want to make
+        depth (int): how much further play we make
         alpha (float, optional): current highest value. Defaults to float("-inf").
         beta (float, optional): current smallest value. Defaults to float("inf").
 
@@ -137,44 +134,45 @@ def negamax(
     """
     dark, light, pawn, player = find_pawn(boardState)
 
-    # if longer than time_limit, we play the best move found in the depth, same thing with game over or no play possible
     if (time.time() - start_time > time_limit) or game_over(dark, light, list_move) or depth == 0:
         return -heurestic(dark, light, player, list_move), None
 
-    the_value, the_move = float("-inf"), None  # best score initialize
+    the_value, the_move = float("-inf"), None
 
     for move in list_move:
-        # find the pawn and moves available of a similated game
+        # find the pawn and moves available of a simulated game
         successor = apply(boardState, move, player, pawn)
         new_dark, new_light, new_pawn, new_player = find_pawn(successor)
         new_list_move = possible_move(new_dark, new_light, new_pawn, new_player)
-        # iteration, we now take the place of the opponent
-        value, _ = negamax(successor, new_list_move, start_time, time_limit, depth - 1, -beta, -alpha)
+        value, _ = negamax(successor, new_list_move, start_time, time_limit, depth - 1, -beta, -alpha)  # iteration
+
         # find the best move (closest ot 0 value)
         if value > the_value:
             the_value, the_move = value, move
         alpha = max(alpha, the_value)
 
         if alpha >= beta:
-            break  # cut the unessesary branchs
+            break  # cut the unnecessary  branchs
     return -the_value, the_move
 
 
 def move(boardState: dict, strategy: bool, time_limit: float = 2.5) -> list:
-    """take the board information and choose a move with the final position
+    """choose the final position
 
     Args:
-        boardState (dict): current state of the game (color of the tile, pawns and current player)
+        boardState (dict): state of the game
+        strategy (bool): algorithm (True) or random (False)
+        time_limit (float, optional): must send after this limit. Defaults to 2.5.
 
     Returns:
-        list: the tile we want to move from our current position
+        list: pawn tile and final tile coordonates
     """
+
     dark, light, pawn, player = find_pawn(boardState)
     list_move = possible_move(dark, light, pawn, player)
     best_move_saved = []
 
     if strategy:
-        # print("SMART MOVE")
         start_time = time.time()
         for depth in range(1, 20):  # 20 is arbitrary (usually around depth 8-12)
             if time.time() - start_time > time_limit:
@@ -195,7 +193,6 @@ def move(boardState: dict, strategy: bool, time_limit: float = 2.5) -> list:
         print(f"depth: {depth}, best move: {final_move} ")
     else:
         final_move = random.choice(list_move)
-        # print("STUPID MOVE")
 
     print(f"starting position:{pawn}")
 
